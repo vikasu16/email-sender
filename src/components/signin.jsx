206ca820fc9fd646bea57e346da6a6a0895a53ed
Vikas Upadhayay <vikas.upadhayay@cginfinity.com>
2024-03-22 22:19:00 +0530

    Added a new text on login page

--- src/components/signin.jsx
+++ src/components/signin.jsx
@@ -38,6 +38,7 @@ const signin = () => {
                 </div>
                 <div className="bg-white py-12 px-10 shadow sm:rounded-lg sm:px-10 text-center">
                     <h5 className="py-3 text-2xl">Welcome</h5>
+                    <h5 className="py-3">Log in to continue to EmailSender.</h5>
                     <button className="w-full flex justify-center items-center gap-2 py-3 px-4 border border-black rounded font-light text-md text-gray-900 hover:bg-gray-200 focus:outline-none focus:ring-2 " onClick = { () => authenicate() }>
                         <img src={GoogleIcon} className="w-5 h-5 mr-2" />
                         Continue with Google
