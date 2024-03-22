0dd5bd09c83f354eb1973a4a4f4858bbd212b79e
Vikas Upadhayay <vikas.upadhayay@cginfinity.com>
2024-03-22 21:55:21 +0530

    Styled the Login page

--- src/components/signin.jsx
+++ src/components/signin.jsx
@@ -1,6 +1,6 @@
 import { signInWithPopup, GoogleAuthProvider } from "firebase/auth";
 import { auth } from '../middleware/Auth';
-
+import GoogleIcon from "../assets/google.svg";
 
 const provider = new GoogleAuthProvider();
 provider.addScope("https://mail.google.com/");
@@ -32,11 +32,16 @@ const signin = () => {
             <div className="w-full md:w-2/5 bg-black flex justify-center items-center h-screen max-sm:hidden max-md:hidden">
                 <h1 className="text-4xl font-bold mb-4 text-white">Email Sender</h1>
             </div>
-            <div className="w-full h-screen md:w-3/5 bg-gray-900 flex justify-center items-center">
-            <div className="bg-white py-12 px-4 shadow sm:rounded-lg sm:px-10">
-                <button onClick = { () => authenicate() }>
-                    sign in with google
-                </button>
+            <div className="w-full h-screen md:w-3/5 bg-gray-900 flex flex-col justify-center items-center">
+                <div>
+                    <h2 className="text-white font-bold text-2xl p-8">Log In</h2>
+                </div>
+                <div className="bg-white py-12 px-10 shadow sm:rounded-lg sm:px-10 text-center">
+                    <h5 className="py-3 text-2xl">Welcome</h5>
+                    <button className="w-full flex justify-center items-center gap-2 py-3 px-4 border border-black rounded font-light text-md text-gray-900 hover:bg-gray-200 focus:outline-none focus:ring-2 " onClick = { () => authenicate() }>
+                        <img src={GoogleIcon} className="w-5 h-5 mr-2" />
+                        Continue with Google
+                    </button>
                 </div>
             </div>
         </div>
