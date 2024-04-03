f4f8143484ae712b45f840295243eda7e75a341a
Vikas Upadhayay <vikas.upadhayay@cginfinity.com>
2024-04-04 00:00:44 +0530

    Updated Env

--- src/middleware/Auth.js
+++ src/middleware/Auth.js
@@ -3,13 +3,13 @@ import { getAuth } from "firebase/auth";
 
 
 const firebaseConfig = {
-  apiKey: "AIzaSyAkSDan4yTRNDxiQJe5-VFhwqz-iiO5vI8",
-  authDomain: "authapplication-1edfc.firebaseapp.com",
-  projectId: "authapplication-1edfc",
-  storageBucket: "authapplication-1edfc.appspot.com",
-  messagingSenderId: "143450948923",
-  appId: "1:143450948923:web:c1ce99298db551c6c2a31e",
-  measurementId: "G-4S9T4TMR2E"
+  apiKey: process.env.REACT_APP_API_KEY,
+  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_MESSING_SENDER_ID,
+  appId: process.env.REACT_APP_APP_ID,
+  measurementId: process.env.REACT_APP_MEASUREMENT_ID
 };
 
 
--- vite.config.js
+++ vite.config.js
@@ -8,7 +8,13 @@ export default defineConfig(({ mode }) => {
     define: {
       'process.env.REACT_APP_GOOGLE_SCOPE': JSON.stringify(env.REACT_APP_GOOGLE_SCOPE),
       'process.env.REACT_APP_GOOGLE_SERVER': JSON.stringify(env.REACT_APP_GOOGLE_SERVER),
-      'process.env.REACT_APP_API_SERVER': JSON.stringify(env.REACT_APP_API_SERVER)
+      'process.env.REACT_APP_API_SERVER': JSON.stringify(env.REACT_APP_API_SERVER),
+      'process.env.REACT_APP_API_KEY': JSON.stringify(env.REACT_APP_API_KEY),
+      'process.env.REACT_APP_PROJECT_ID': JSON.stringify(env.REACT_APP_PROJECT_ID),
+      'process.env.REACT_APP_STORAGE_BUCKET': JSON.stringify(env.REACT_APP_STORAGE_BUCKET),
+      'process.env.REACT_APP_MESSING_SENDER_ID': JSON.stringify(env.REACT_APP_MESSING_SENDER_ID),
+      'process.env.REACT_APP_APP_ID': JSON.stringify(env.REACT_APP_APP_ID),
+      'process.env.REACT_APP_MEASUREMENT_ID': JSON.stringify(env.REACT_APP_MEASUREMENT_ID),
     },
     plugins: [react()],
   }
